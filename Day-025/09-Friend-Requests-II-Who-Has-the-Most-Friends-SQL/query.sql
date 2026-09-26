1a6f8f14af195db8e9285901dbced0b89e3a62ae
vinayakzirange <vinayakzirange@gmail.com>
2026-09-26 22:16:24 +0530

    Day 025 - Problem: 09-Friend-Requests-II-Who-Has-the-Most-Friends-SQL

---
+++ Day-025/09-Friend-Requests-II-Who-Has-the-Most-Friends-SQL/query.sql
@@ -0,0 +1,16 @@
+-- Problem: Friend Requests II: Who Has the Most Friends (LeetCode 602)
+-- Language: SQL
+-- Difficulty: Medium
+
+WITH AllFriends AS (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+)
+SELECT 
+    id, 
+    COUNT(*) AS num
+FROM AllFriends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
